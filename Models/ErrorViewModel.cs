fdb290835292eb4e74b1298dd5f36de71692fb1a
Rafael Liendo <rjld2409@hotmail.com>
2021-01-19 23:05:31 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppCoreGIT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
